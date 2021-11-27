01eb5be256e5d68c99d27caf4ae2ad5b37999e29
Anand_Maurya <79273275+coderanandmaurya@users.noreply.github.com>
2021-11-27 10:44:17 +0530

    Create datatype.cpp

---
+++ datatype.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int x;
+    long int y;
+    char a;
+    float f;
+    double d;
+    cin >> x>>y>>a>>f>>d;
+    cout<<x<<"\n"<<y<<"\n"<<a<<"\n"<<f<< "\n"<<d;
+    return 0;  
+}
